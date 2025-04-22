cef9c8abed91cbd2402c41559cf3fdca4bbe0a7b
Dhiv Joseph <dhiv.joseph@gmail.com>
2025-04-22 20:42:12 +1000

    feat: add code generation

---
+++ scripts/generate.ps1
@@ -0,0 +1,15 @@
+# Create necessary directories if they don't exist
+$null = New-Item -ItemType Directory -Force -Path internal/domain/models
+$null = New-Item -ItemType Directory -Force -Path internal/adapters/api
+$null = New-Item -ItemType Directory -Force -Path internal/adapters/client
+
+Write-Host "Generating API models..."
+oapi-codegen --config=.codegen/models.yaml ./palaestra-api-v1.yaml
+
+Write-Host "Generating API server..."
+oapi-codegen -generate "types" --config=.codegen/server.yaml ./palaestra-api-v1.yaml
+
+Write-Host "Generating API client..."
+oapi-codegen -generate "types" --config=.codegen/client.yaml ./palaestra-api-v1.yaml
+
+Write-Host "Generation complete!"
